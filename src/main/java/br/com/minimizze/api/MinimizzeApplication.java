51ce5d1e0ea8646e2ac38536c89343957baed1ed
benjamin <vitor.benjamin7@gmail.com>
2018-06-23 13:18:53 -0300

    Commit inicial com os arquivos

---
+++ src/main/java/br/com/minimizze/api/MinimizzeApplication.java
@@ -0,0 +1,12 @@
+package br.com.minimizze.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimizzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimizzeApplication.class, args);
+	}
+}
