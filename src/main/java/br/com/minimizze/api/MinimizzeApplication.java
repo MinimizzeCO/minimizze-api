792dc0a0e45129f8b81f0ce538af723be9134cf3
benjamin <vitor.benjamin7@gmail.com>
2018-09-01 15:32:09 -0300

    Teste

--- src/main/java/br/com/minimizze/api/MinimizzeApplication.java
+++ src/main/java/br/com/minimizze/api/MinimizzeApplication.java
@@ -2,10 +2,8 @@ package br.com.minimizze.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("br.com.minimizze.api.entities")
 public class MinimizzeApplication {
 
 	public static void main(String[] args) {
