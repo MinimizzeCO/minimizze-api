203d9cac189fc68956fff011c58e0794592deab9
vitorbenjamin <vitor.benjamin7@gmail.com>
2018-10-11 18:44:50 -0300

    Vilaça

--- src/main/java/br/com/minimizze/api/dtos/CategoriaProdutoDto.java
+++ src/main/java/br/com/minimizze/api/dtos/CategoriaProdutoDto.java
@@ -2,14 +2,6 @@ package br.com.minimizze.api.dtos;
 
 
 import java.util.List;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-
 import javax.validation.constraints.NotNull;
 /**
  * @author Vitor
@@ -18,20 +10,13 @@ import javax.validation.constraints.NotNull;
  *
  */
 
-@Entity
 public class CategoriaProdutoDto extends AbstractEntityDto {
 
-	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@NotNull
 	private String nome;
-	
-	@ManyToOne
 	private CategoriaProdutoDto categoriaProdutoPai; 
-	
-	@OneToMany(mappedBy="categoriaProdutoPai")
 	private List<CategoriaProdutoDto> subcategoria;
 
 	public Long getId() {
